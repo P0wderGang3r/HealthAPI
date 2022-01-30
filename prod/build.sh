405a55586b63d046151445844c5aec5276f0e043
P0wderGang3r <skopetskiy.ts@gmail.com>
2022-01-30 19:15:24 +1000

    Делаем велосипеды #2

--- prod/build.sh
+++ prod/build.sh
@@ -5,7 +5,7 @@ sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev p
 pip3 install virtualenv
 sudo python3 -m virtualenv HealthAPIEnv
 source HealthAPIEnv/bin/activate
-pip install wheel uwsgi flask
+sudo pip install wheel uwsgi flask
 deactivate
 sudo git fetch
 sudo git pull
