588a63c23fa4d270bcb60409da1dd3734fd07174
P0wderGang3r <skopetskiy.ts@gmail.com>
2022-01-31 19:57:32 +1000

    .env fixes

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,4 +1,4 @@
 git add -A
 git commit -a
 git push
-ssh FlaskMachineUser@dvfuflaskmachine.westus2.cloudapp.azure.com 'cd HealthAPI/dev ; ./build.sh ; sudo systemctl restart HealthAPIdev ; cp env_dev/.env ./ ; exit'
+ssh FlaskMachineUser@dvfuflaskmachine.westus2.cloudapp.azure.com 'cd HealthAPI/dev ; ./build.sh ; sudo systemctl restart HealthAPIdev ; cp -f env_dev/.env ./ ; exit'
--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,4 +1,4 @@
 git add -A
 git commit -a
 git push
-ssh FlaskMachineUser@dvfuflaskmachine.westus2.cloudapp.azure.com 'cd HealthAPI/prod ; ./build.sh ; sudo systemctl restart HealthAPIprod ; cp env_prod/.env ./ ; exit'
+ssh FlaskMachineUser@dvfuflaskmachine.westus2.cloudapp.azure.com 'cd HealthAPI/prod ; ./build.sh ; sudo systemctl restart HealthAPIprod ; cp -f env_prod/.env ./ ; exit'
