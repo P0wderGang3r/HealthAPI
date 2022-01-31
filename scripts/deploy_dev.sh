456b4fcc64362d1e04464e5e523a129d8e3abb59
P0wderGang3r <skopetskiy.ts@gmail.com>
2022-01-31 20:10:08 +1000

    Точно финал

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -a
+git commit -a -m "deploy_dev"
 git push
 ssh FlaskMachineUser@dvfuflaskmachine.westus2.cloudapp.azure.com 'cd HealthAPI/dev ; ./build.sh ; sudo systemctl restart HealthAPIdev ; cp -f env_dev/.env ./ ; exit'
--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -a
+git commit -a -m "deploy_prod"
 git push
 ssh FlaskMachineUser@dvfuflaskmachine.westus2.cloudapp.azure.com 'cd HealthAPI/prod ; ./build.sh ; sudo systemctl restart HealthAPIprod ; cp -f env_prod/.env ./ ; exit'
