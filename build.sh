f2b8caf664bdb600d2cd323934f3e221b50a5132
P0wderGang3r <skopetskiy.ts@gmail.com>
2022-01-31 19:46:00 +1000

    build fix

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt install nginx
 sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-pip3 install virtualenv  python-dotenv
+pip3 install virtualenv
 python3 -m virtualenv HealthAPIenv
 source HealthAPIenv/bin/activate
-pip install wheel uwsgi flask
+pip install wheel uwsgi flask python-dotenv
 deactivate
 sudo git fetch
 sudo git pull
